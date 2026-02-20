14125363a5ae2e25ba2e62f4fb70ea40b9de2438
Angelo Araya Villanueva <angeloaraya11@gmail.com>
2026-02-20 11:44:09 -0500

    adjust to index for dealing with new format

--- api/index.py
+++ api/index.py
@@ -4,19 +4,23 @@ import redis
 
 app = FastAPI()
 
+# Conexión a Redis
 REDIS_URL = os.getenv("REDIS_URL")
 r = redis.from_url(REDIS_URL)
 
 @app.get("/api")
 def get_status():
     try:
-        logs = r.lrange("system_logs", 0, 10)
+        logs = r.zrevrange("orchestrator_telemetry", 0, -1)
+        
         logs_decoded = [log.decode('utf-8') for log in logs]
         
         return {
             "status": "online",
             "project": "Ephyphonic",
             "owner": "Angelo Araya",
+            "retention_policy": "7_days_dynamic",
+            "total_logs": len(logs_decoded),
             "last_events": logs_decoded
         }
     except Exception as e:
